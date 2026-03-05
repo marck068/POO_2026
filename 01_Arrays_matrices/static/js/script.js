9e99967d3e28a3de513a45c2945be0e774e0e7ae
marck068 <marcelorios@liceovvh.cl>
2026-03-05 09:22:44 -0300

    asd

---
+++ 01_Arrays_matrices/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conectando a js...")
+
+/* Ejercicio 1: El Mensaje Desordenado (Push y Pop)
+Instrucciones: Crea una función llamada construirFrase. Dentro de ella, define el arreglo let palabras = ["programar", "es"];.
+Usa .push() para agregar "increíble" al final.
+Usa .push() para agregar "borrar" al final.
+Usa .pop() para eliminar "borrar".
+Muestra con alert() la frase unida por espacios. */
+
+let palabras = ["programar", "es"];
