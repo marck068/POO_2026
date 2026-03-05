6b732b4f3a26ff8953017e825bade8194a10a32a
marck068 <marcelorios@liceovvh.cl>
2026-03-05 15:08:58 -0300

    klkl

---
+++ 02_condicionales/static/js/script.js
@@ -0,0 +1,23 @@
+/* 1. El Portero Digital
+Crea una variable edad. Si la edad es 18 o más, muestra por consola: "Acceso permitido a la App".
+Si es menor, muestra: "Acceso denegado: necesitas ser mayor de edad".
+ */
+
+function porteroDigital() {
+    let edad = parseInt(prompt("Ingrese su edad:"))
+    if (edad >= 18) {
+        alert("Acceso permitido a la App")
+    } else{
+        alert("Acceso denegado: necesitas ser mayor de edad")
+    }
+}
+
+/* 2. Validador de Nombres
+Declara una variable nombre.
+Si el nombre no está vacío, crea un arreglo llamado usuarios y agrega el nombre usando .push().
+Si está vacío, muestra: "Error: El nombre no puede estar en blanco".
+ */
+
+function validarNombre() {
+    let nombre = parseInt("Ingrese su nombre")
+}
