890cfb626f94c8663302b3ae5faac527891bb851
marck068 <marcelorios@liceovvh.cl>
2026-03-11 12:25:51 -0300

    asdasd

--- 02_condicionales/static/js/script.js
+++ 02_condicionales/static/js/script.js
@@ -1,13 +1,13 @@
 /* 1. El Portero Digital
 Crea una variable edad. Si la edad es 18 o más, muestra por consola: "Acceso permitido a la App".
 Si es menor, muestra: "Acceso denegado: necesitas ser mayor de edad".
- */
+*/
 
 function porteroDigital() {
     let edad = parseInt(prompt("Ingrese su edad:"));
     if (edad >= 18) {
         alert("Acceso permitido a la App")
-    } else{
+    } else {
         alert("Acceso denegado: necesitas ser mayor de edad")
     };
 };
@@ -16,7 +16,7 @@ function porteroDigital() {
 Declara una variable nombre.
 Si el nombre no está vacío, crea un arreglo llamado usuarios y agrega el nombre usando .push().
 Si está vacío, muestra: "Error: El nombre no puede estar en blanco".
- */
+*/
 
 function validarNombre() {
     let nombre = prompt("Ingrese su nombre");
@@ -32,10 +32,52 @@ function validarNombre() {
 /* 3. El Filtro de Duplicados
 Tienes esta lista: let productos = ["celular", "teclado", "mouse"];
 Declara una variable nuevoProducto.
-Si el producto no está en la lista (usa .includes()), agrégalo al final y muestra la lista completa.
+Si el producto no está en la lista (usa .includes()), agrégalo al final
+ y muestra la lista completa.
 Si ya existe, muestra: "El producto ya está en el inventario".
 */
 
 function filtroDuplicado() {
-    
+    let productos = ["celular", "teclado", "mouse"];
+    let nuevoProducto = "celular";
+
+    if (productos.includes(nuevoProducto)) {
+        alert("El producto ya está en el inventario");
+    } else {
+        alert("El producto no está en el inventario");
+    }
+};
+
+/* 4. Control de Stock Máximo
+Imagina que una bodega solo tiene espacio para 5 cajas.
+let bodega = ["caja1", "caja2", "caja3", "caja4", "caja5", "caja6"];
+Si el tamaño de la bodega (.length) es mayor a 5, elimina la última caja con .pop()
+ y muestra: "Bodega llena, eliminando último ingreso".
+Si es 5 o menos, muestra: "Espacio disponible".
+*/
+
+let bodega = ["caja1", "caja2", "caja3", "caja4", "caja5", "caja6"];
+function stockMaximo() {
+    if (bodega.length <= 5) {
+        alert("Espacio disponible")
+    } else {
+        alert("Bodega llena, eliminando último ingreso")
+        bodega.pop();
+    }
+}
+
+
+/* 5. Limpieza de Datos
+A veces recibimos datos basura. let colaEspera = ["error_404", "Juan", "Sofía"];
+Si el primer elemento (índice 0) es igual a "error_404", elimínalo usando .shift()
+ y muestra la cola limpia.
+Si no, muestra: "La lista de espera está correcta".
+*/
+
+function limpiezaDatos() {
+    let colaEspera = ["error_404", "Juan", "Sofía"];
+    if (colaEspera[0] == "error_404"){
+        colaEspera.shift()
+    }
+    alert(`Cola de espera limpia: ${colaEspera.join(" - ")}`)
 }
