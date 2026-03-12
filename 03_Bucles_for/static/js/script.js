2abc4fd4ebafe0d7d2403f90e4d4f0d277ea15e9
marck068 <marcelorios@liceovvh.cl>
2026-03-12 11:11:20 -0300

    asdasd

--- 03_Bucles_for/static/js/script.js
+++ 03_Bucles_for/static/js/script.js
@@ -28,11 +28,12 @@ function listaAutomatica() {
     let curso = ["Ana", "Diego", "Sofía", "Matias"];
     const container = document.getElementById('resultadoContainer2');
     const resultado = document.getElementById('result2');
+    resultado.textContent = "";
     for (let i = 0; i < curso.length; i++) {
         resultado.textContent += `${curso[i]}, `
     }
     container.classList.remove('d-none');
-}
+};
 
 
 /* 3. Buscador de Aprobados (Escala 1 a 7)
@@ -42,9 +43,17 @@ Recorre el arreglo con un for. Si la nota es 4.0 o superior, suma 1 al contador.
 Al terminar el ciclo, muestra en el párrafo pantalla: "Total de alumnos aprobados: [número]".
  */
 function buscadorAprobados() {
+    let notas = [3.5, 6.2, 5.0, 2.8, 4.5, 7.0];
+    let contadorAprobados = 0;
     const container = document.getElementById('resultadoContainer3');
     const resultado = document.getElementById('result3');
-    container.classList.remove('d-none')
+    for (let i = 0; i < notas.length; i++){
+        if (notas[i] >= 4.0){
+            contadorAprobados++
+        }
+    }
+    resultado.textContent = `Total de alumnos aprobados: ${contadorAprobados}`;
+    container.classList.remove('d-none');
 };
 
 
