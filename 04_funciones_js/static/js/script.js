f3fbc098d086896354f04d8d6920218bbfb65c26
marck068 <marcelorios@liceovvh.cl>
2026-03-17 12:55:39 -0300

    asdasd

---
+++ 04_funciones_js/static/js/script.js
@@ -0,0 +1,20 @@
+console.log("JS conectado")
+
+
+/* 1. La Anatomía de una Función
+Una función es un bloque de código empaquetado y guardado con un nombre, que solo se ejecuta cuando tú se lo ordenas.
+¿Cómo se escribe?
+JavaScript
+// 1. Declaración: Creamos al trabajador y le enseñamos qué hacer.
+function saludar() {
+    console.log("¡Hola! Bienvenido al sistema.");
+}
+// 2. Ejecución (Llamada): Le ordenamos al trabajador que trabaje.
+saludar(); // Esto imprimirá el mensaje en la consola.
+saludar(); // ¡Podemos llamarlo todas las veces que queramos!
+ */
+
+function listarAsistencia() {
+    const input = document.getElementById("input1").value;
+    // input = parseInt(input); --> Transformar 
+};
